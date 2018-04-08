0c32447858ec552ec723eea665a3c5fe502dbc6d
Michael Mancha <mancham@hawaii.edu>
2018-04-07 22:55:29 -1000

    Finished first tab of project two (current problems: tab bar not showing up, third tab not able to create IB outlets)

--- ProjectTwoRemake/ViewController.swift
+++ ProjectTwoRemake/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelOne: UILabel!
+    @IBOutlet var labelTwo: UILabel!
+    @IBOutlet var labelThree: UILabel!
+    @IBOutlet var labelFour: UILabel!
+    @IBOutlet var labelFive: UILabel!
+    @IBOutlet var textViewOne: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    self.labelOne.text = "My Name is:"
+    self.labelTwo.text = "Michael Mancha"
+    self.labelThree.text = "My Major is:"
+    self.labelFour.text = "Psychology"
+    self.labelFive.text = "My Spring 2018 Classes are:"
+    self.textViewOne.text = "My classes for this semester are Creative Media 161, Historical Archeology, American History II, Senior Project for Psychology, and Statisitcs"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
